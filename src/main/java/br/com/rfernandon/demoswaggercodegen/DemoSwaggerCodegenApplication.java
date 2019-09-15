02a4a275cecfd7bee3686451f8979cc42967a1a7
rfernandon <r.fernandon@gmail.com>
2019-09-14 21:42:44 -0300

    commit inicial

---
+++ src/main/java/br/com/rfernandon/demoswaggercodegen/DemoSwaggerCodegenApplication.java
@@ -0,0 +1,13 @@
+package br.com.rfernandon.demoswaggercodegen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSwaggerCodegenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSwaggerCodegenApplication.class, args);
+    }
+
+}
